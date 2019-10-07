7a3c5ce3f33b004caecdc10438a4bdc0190e8e9d
Spartfin <fin.07@bk.ru>
2019-10-07 18:46:53 +0300

    Учебный проект: в движении

---
+++ js/dialog.js
@@ -0,0 +1,132 @@
+'use strict';
+(function () {
+  var setupOpen = document.querySelector('.setup-open');
+  var setupClose = window.util.elems.setupPopup.querySelector('.setup-close');
+  var dialogHandle = window.util.elems.setupPopup.querySelector('.upload');
+  var POPUP_DEFAULT = {
+    x: window.util.elems.setupPopup.style.left,
+    y: window.util.elems.setupPopup.style.top
+  };
+
+  /**
+   * @description Показывает модальное окно настройки персонажа
+   */
+  var openPopup = function () {
+    window.util.elems.setupPopup.classList.remove('hidden');
+    window.util.elems.setupPopup.style.left = POPUP_DEFAULT.x;
+    window.util.elems.setupPopup.style.top = POPUP_DEFAULT.y;
+  };
+
+  /**
+   * @description Закрывает модальное окно настройки персонажа
+   */
+  var closePopup = function () {
+    window.util.elems.setupPopup.classList.add('hidden');
+  };
+
+  /**
+   * Закрытия окна при нажатии на ESC
+   * @param {Event} evt
+   */
+  var onPopupEscPress = function (evt) {
+    window.util.isEscEvent(evt, closePopup);
+  };
+
+  /**
+   * @description Событие открытия окна настройки персонажа при клике мышью на иконку пользователя
+   */
+  setupOpen.addEventListener('click', function () {
+    openPopup();
+  });
+
+  /**
+   * Событие открытия окна настройки персонажа при нажание Enter при фокусе на иконке пользователя
+   * @param {Event} evt
+   */
+  setupOpen.addEventListener('keydown', function (evt) {
+    window.util.isEnterEvent(evt, openPopup);
+  });
+
+  /**
+   * @description Событие закрытия окна настройки персонажа при клике мышью на крестик в меню настройки персонажа
+   */
+  setupClose.addEventListener('click', function () {
+    closePopup();
+  });
+
+  /**
+   * Событие закрытия окна настройки персонажа при нажание Enter при фокусе на крестике в меню настройки персонажа
+   * @param {Event} evt
+   */
+  setupClose.addEventListener('keydown', function (evt) {
+    window.util.isEnterEvent(evt, closePopup);
+  });
+
+  /**
+   * @description Событие закрытия окна настройки персонажа при нажатии на ECS
+   */
+  document.addEventListener('keydown', onPopupEscPress);
+
+  /**
+   * Событие перетаскивания окна, активируется при нажатии левой кнопки мыши на аватар пользователя в окне персонажа
+   * @param {Event} evt
+   */
+  dialogHandle.addEventListener('mousedown', function (evt) {
+    evt.preventDefault();
+
+    var startCoordinates = {
+      x: evt.clientX,
+      y: evt.clientY
+    };
+
+    var dragged = false;
+
+    /**
+     * Функция, вычисляет насколько произошло смещение при движении мышью и устанавливает новые координаты для окна персонажа
+     * @param {Event} moveEvt
+     */
+    var onMouseMove = function (moveEvt) {
+      moveEvt.preventDefault();
+
+      dragged = true;
+
+      var shift = {
+        x: startCoordinates.x - moveEvt.clientX,
+        y: startCoordinates.y - moveEvt.clientY
+      };
+
+      startCoordinates = {
+        x: moveEvt.clientX,
+        y: moveEvt.clientY
+      };
+
+      window.util.elems.setupPopup.style.top = (window.util.elems.setupPopup.offsetTop - shift.y) + 'px';
+      window.util.elems.setupPopup.style.left = (window.util.elems.setupPopup.offsetLeft - shift.x) + 'px';
+
+    };
+
+    /**
+     * Функция, убирает конфликт между установкой нового аватара пользователя и перетаскиванием диалогового окна
+     * @param {Event} upEvt
+     */
+    var onMouseUp = function (upEvt) {
+      upEvt.preventDefault();
+
+      document.removeEventListener('mousemove', onMouseMove);
+      document.removeEventListener('mouseup', onMouseUp);
+
+      if (dragged) {
+        var onClickPreventDefault = function (draggedEvt) {
+          draggedEvt.preventDefault();
+          dialogHandle.removeEventListener('click', onClickPreventDefault);
+        };
+        dialogHandle.addEventListener('click', onClickPreventDefault);
+      }
+    };
+
+    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mouseup', onMouseUp);
+
+  });
+
+})();
--- js/stat.js
+++ js/stat.js
@@ -1,60 +1,71 @@
 'use strict';
 
-(function () {
+var CLOUD_WIDTH = 420;
+var CLOUD_HEIGHT = 270;
+var CLOUD_X = 100;
+var CLOUD_Y = 10;
+var GAP = 10;
+var BAR_MAX_HEIGTH = 150;
+var BAR_WIDTH = 40;
+var BAR_GAP = 50;
 
-  var CLOUD_WIDTH = 420;
-  var CLOUD_HEIGHT = 270;
-  var CLOUD_X = 100;
-  var CLOUD_Y = 10;
-  var INDENT_X = 150;
-  var INDENT_Y = 20;
-  var GAP = 10;
-  var FONT_GAP = 16;
-  var BAR_GAP = 50;
-  var BAR_HEIGHT = 150;
-  var BAR_WIDTH = 40;
+// Получение максимального числа в массиве
 
-  var renderCloud = function (ctx, x, y, color) {
-    ctx.fillStyle = color;
-    ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
-  };
+var getMaxValue = function (array) {
+  var maxValue = array[0];
 
-  var getMaxElement = function (arr) {
-    var maxElement = arr[0];
-    for (var i = 1; i < arr.length; i++) {
-      if (arr[i] > maxElement) {
-        maxElement = arr[i];
-      }
+  for (var i = 0; i < array.length; i++) {
+    if (array[i] > maxValue) {
+      maxValue = array[i];
     }
-    return maxElement;
-  };
+  }
 
-  window.renderStatistics = function (ctx, names, times) {
-    renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
-    renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
+  return maxValue;
+};
 
-    ctx.font = '16px PT Mono';
-    ctx.fillStyle = '#000';
-    ctx.textBaseline = 'hanging';
-    ctx.fillText('Ура вы победили!', INDENT_X, INDENT_Y);
-    ctx.fillText('Список результатов:', INDENT_X, INDENT_Y + (GAP * 2));
+// Генерация случайной насыщенности в HSL
 
-    var maxTime = getMaxElement(times);
+var getRandomSaturation = function (hueValue, lightness) {
+  var saturation = Math.floor(Math.random() * 101);
 
-    for (var i = 0; i < names.length; i++) {
-      var barHeight = BAR_HEIGHT * times[i] / maxTime;
+  return 'hsl(' + hueValue + ', ' + saturation + '%,' + lightness + '%)';
+};
 
-      ctx.fillStyle = '#000';
-      ctx.fillText(names[i], CLOUD_X + BAR_GAP + i * 2 * BAR_GAP, CLOUD_HEIGHT - GAP * 2);
-      ctx.fillText(Math.round(times[i]), CLOUD_X + BAR_GAP + i * 2 * BAR_GAP, CLOUD_Y + CLOUD_HEIGHT - 2 * GAP - FONT_GAP - barHeight - GAP * 2);
-      ctx.fillStyle = 'rgba(0, 0, 255,' + parseFloat(Math.random().toPrecision(1)) + ')';
+// Создание элемнетов отрисовки статистики
 
-      if (names[i] === 'Вы') {
-        ctx.fillStyle = 'rgba(255, 0, 0, 1)';
-      }
+var renderCloud = function (ctx, x, y) {
+  ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
+  ctx.fillRect(x + GAP, y + GAP, CLOUD_WIDTH, CLOUD_HEIGHT);
+  ctx.fillStyle = 'rgba(255, 255, 255, 1';
+  ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 
-      ctx.fillRect(CLOUD_X + BAR_GAP + i * 2 * BAR_GAP, CLOUD_Y + CLOUD_HEIGHT - 2 * GAP - FONT_GAP - barHeight, BAR_WIDTH, barHeight);
-    }
-  };
+  ctx.font = '16px PT Mono';
+  ctx.fillStyle = '#000';
+  ctx.textBaseline = 'hanging';
+  ctx.fillText('Ура вы победили!', x + GAP * 3, y + GAP * 2);
+  ctx.fillText('Список результатов:', x + GAP * 3, y + GAP * 4);
+};
 
-})();
+var renderВarGraph = function (ctx, namesArray, timesArray, elementIndex) {
+  var maxTime = getMaxValue(timesArray);
+
+  var barHeight = BAR_MAX_HEIGTH / maxTime * Math.ceil(timesArray[elementIndex]);
+  var barX = CLOUD_X + BAR_WIDTH + BAR_GAP * 2 * elementIndex;
+  var barY = CLOUD_Y + GAP * 8 + (BAR_MAX_HEIGTH - barHeight);
+
+  ctx.fillStyle = 'rgba(0, 0, 0, 1)';
+  ctx.fillText(namesArray[elementIndex], barX, CLOUD_HEIGHT - GAP * 2);
+  ctx.fillText(Math.ceil(timesArray[elementIndex]), barX, barY - GAP * 2);
+  ctx.fillStyle = namesArray[elementIndex].toUpperCase() === 'ВЫ' ? 'rgba(255, 0, 0, 1)' : getRandomSaturation(240, 50);
+  ctx.fillRect(barX, barY, BAR_WIDTH, barHeight);
+};
+
+// Отрисовка статистики
+
+window.renderStatistics = function (ctx, names, times) {
+  renderCloud(ctx, CLOUD_X, CLOUD_Y);
+
+  for (var i = 0; i < names.length; i++) {
+    renderВarGraph(ctx, names, times, i);
+  }
+};
